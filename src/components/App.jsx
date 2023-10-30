0f4f5910cee3b7c50ac6eb488898d6910f258c07
Serhii Pochtovyk <sergey.po4tovik@gmail.com>
2023-10-30 17:11:23 +0200

    create searchbar;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+
+class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    isLoading: false,
+    showModal: false,
+    selectedImage: '',
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleSearch} />
+      </div>
+    );
+  }
+}
+
+export default App;
